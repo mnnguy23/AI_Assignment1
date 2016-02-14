a8fe6742681db642e912e29a7a1a358b551c963c
jtran93 <johnathan_tran93@yahoo.com>
2016-02-14 17:19:03 -0600

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include "Genetic.h"
+#include "Mutation.h"
+
+int main()
+{
+	Genetic population;
+	Mutation mutation;
+	std::vector < std::vector <int> > p;
+
+	population.createInitialPopulation(p);
+	population.printPopulation(p);
+
+	mutation.generateMutation(p);
+	population.printPopulation(p);
+
+	return (0); 
+}
