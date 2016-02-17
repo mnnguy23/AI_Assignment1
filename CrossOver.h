1094b92ab23d9419c8be40efb5d89781929eb4a1
jtran93 <johnathan_tran93@yahoo.com>
2016-02-16 19:54:33 -0600

    Create CrossOver.h

---
+++ CrossOver.h
@@ -0,0 +1,22 @@
+#ifndef CrossOver_h
+#define CrossOver_h
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+
+class CrossOver
+{
+	public:
+		std::vector<int> crossoverSelection(int crossOverRate);
+		std::vector< std::vector <int> >crossOver (std::vector< std::vector<int> >population, std::vector<int> crossOverselector);
+		std::vector< std::vector<int> > crossoverMask(std::vector< std::vector<int> > population, std::vector<int> parentSelection);
+		int addToPopulationFromShuffler(int counter, std::vector< std::vector<int> > sibling);
+		void addToPopulationFromRemaining(std::vector< std::vector<int> > population, int counter);
+	private:
+		std::vector< std::vector<int> > newPopulation;
+		std::vector<int> remainingPopulation;
+};
+#endif
